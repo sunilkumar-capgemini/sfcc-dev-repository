a8cfc141fc6ece749d97cddd1239f9755282b087
PrabhuMMoorthy <mprabhumoorthy@gmail.com>
2017-03-29 13:08:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from urllib.error import HTTPError
 
 import json
 import os
+import re
 
 from flask import Flask
 from flask import request
@@ -34,6 +35,24 @@ def webhook():
     r.headers['Content-Type'] = 'application/json'
     return r
 
+	
+def suggestDeodrant(condition, person, city):
+    humidWeatherList = ['tornado','tropical storm','hurricane','severe thunderstorms','thunderstorms','mixed rain and snow','mixed rain and sleet','mixed snow and sleet','freezing drizzle','drizzle','freezing rain','showers','showers','snow flurries','light snow showers','blowing snow','snow','hail','sleet','mixed rain and hail','thundershowers','snow showers','isolated','thundershowers'];
+    hotWeatherList = ['dust','foggy','haze','smoky','blustery','windy','cold','cloudy','mostly cloudy (night)','mostly cloudy (day)','partly cloudy (night)','partlycloudy (day)','clear (night)','sunny','fair (night)','fair (day)','hot','isolated thunderstorms','scattered thunderstorms','scattered thunderstorms','scattered showers','heavy snow','scattered snow showers','heavy snow','partly cloudy'];
+    if(condition in humidWeatherList):
+     print('humid')
+     men = 'Men'
+     if(person.lower()==men.lower()):
+      condition = 'Hmmm.. The weather in '+city+' looks '+condition+'. I suggest these <a href="/highstreetstorefront/highstreet/en/highstreet-Catalogue/Perfumes/Men-Perfumes/Moist/c/580">Anti-Perspirant Deodrants</a> for ' + person
+     else:
+      condition = 'Hmmm.. The weather in '+city+' looks '+condition+'. I suggest these <a href="/highstreetstorefront/highstreet/en/highstreet-Catalogue/Perfumes/Women-Perfumes/Moist/c/395">Anti-Perspirant Deodrants</a> for ' + person
+    else:
+     print('dry')
+     if(person.lower() == person.lower()):
+      condition = 'Hmmm.. The weather in '+city+' looks '+condition+'. I suggest these <a href="/highstreetstorefront/highstreet/en/highstreet-Catalogue/Perfumes/Men-Perfumes/Dry/c/570">Perfumed Deodrants for</a> ' + person
+     else:
+      condition = 'Hmmm.. The weather in '+city+' looks '+condition+'. I suggest these <a href="/highstreetstorefront/highstreet/en/highstreet-Catalogue/Perfumes/Women-Perfumes/Dry/c/390">Perfumed Deodrants for</a> ' + person
+    return condition
 
 def processRequest(req):
     if req.get("result").get("action") != "yahooWeatherForecast":
@@ -45,7 +64,7 @@ def processRequest(req):
     yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
     result = urlopen(yql_url).read()
     data = json.loads(result)
-    res = makeWebhookResult(data)
+    res = makeWebhookResult(data,req)
     return res
 
 
@@ -59,7 +78,7 @@ def makeYqlQuery(req):
     return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 
-def makeWebhookResult(data):
+def makeWebhookResult(data, req):
     query = data.get('query')
     if query is None:
         return {}
@@ -84,15 +103,22 @@ def makeWebhookResult(data):
 
     # print(json.dumps(item, indent=4))
 
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-
-    print("Response:")
-    print(speech)
+    #speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+    #         ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+	
+    speech = "Hmmm.. It looks " + condition.get('text') + " in " + location.get('city')
+    airesult = req.get("result")
+    parameters = airesult.get("parameters")
+    person = parameters.get('Person')
+    city = parameters.get("geo-city")
+    returnedSpeech = suggestDeodrant(condition.get('text'), person, city)
+    print(returnedSpeech)
+    #print("Response:")
+    #print(speech)
 
     return {
-        "speech": speech,
-        "displayText": speech,
+        "speech": returnedSpeech,
+        "displayText": returnedSpeech,
         # "data": data,
         # "contextOut": [],
         "source": "apiai-weather-webhook-sample"
