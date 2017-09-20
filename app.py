0423d109950b111c35f6f1b047020de0db68d50b
highstreetfashionsteam <highstreetfashionsteam@gmail.com>
2017-09-20 15:07:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def relayRequest(req):
             "source": "python-webhook"
         }
     elif req.get("result").get("action") == "request_name_permission" or req.get("result").get("action") == "read_mind" or req.get("result").get("action") == "actions_intent_PERMISSION" :
-        baseurl = "https://us-central1-namepsychicdemo-6bd8d.cloudfunctions.net/namePsychic/"
+        baseurl = "https://us-central1-highstreet-bot-v4-1.cloudfunctions.net/getNameFunction"
     else:
         baseurl = "http://34.203.152.187/highstreetcommercewebservices/v2/highstreet/webhook/"
     
